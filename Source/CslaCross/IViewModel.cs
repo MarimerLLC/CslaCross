8ac3613e3790383570cf04853f34e9720bcfe218
Kevin Ford <bowman74@hotmail.com>
2014-04-01 20:34:15 -0400

    Initial Checkin of CslaCross

---
+++ Source/CslaCross/IViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CslaCross
+{
+    /// <summary>
+    /// Defines a CSLA .NET viewmodel
+    /// object.
+    /// </summary>
+    public interface IViewModel
+    {
+        /// <summary>
+        /// Gets or sets the Model property
+        /// of the viewmodel object.
+        /// </summary>
+        object Model { get; set; }
+    }
+}
